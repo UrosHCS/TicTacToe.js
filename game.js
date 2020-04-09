9b57bcbac8550dfea73563b1c4eeb1776dfe74c9
UrosHCS <uroshcs@gmail.com>
2020-04-09 20:48:54 +0200

    css grid

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var COLOR_VICTORY = "#D500F9";
+
 var gameMatrix; //3x3 matrix of number representations (0, 1, -1)
 var divMatrix = setupDivMatrix(); //3x3 matrix of labels (fields)
 var gameOver; //boolean
@@ -9,122 +11,121 @@ var playerScore = 0;
 var computerScore = 0;
 
 function incrementPlayerScore() {
-    playerScore++;
-    playerScoreElement.innerHTML = playerScore;
+  playerScore++;
+  playerScoreElement.innerHTML = playerScore;
 }
 function incrementComputerScore() {
-    computerScore++;
-    computerScoreElement.innerHTML = computerScore;
+  computerScore++;
+  computerScoreElement.innerHTML = computerScore;
 }
 
-
 function setupDivMatrix() {
-    var table = document.getElementById("table");
-    var matrix = [[],[],[]];
-    for (var row = 0; row < 3; row++) {
-        for (var col = 0; col < 3; col++) {
-            matrix[row][col] = table.getElementsByClassName("row")[row].getElementsByClassName("table-cell")[col];
-        }
+  //   var table = document.getElementById("table");
+  var matrix = [[], [], []];
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      // matrix[row][col] = table.getElementsByClassName("row")[row].getElementsByClassName("table-cell")[col];
+      matrix[row][col] = document.getElementById(row + "-" + col);
     }
-    return matrix;
+  }
+  return matrix;
 }
 
 function _newGame() {
-    gameMatrix = [[0,0,0],[0,0,0],[0,0,0]]; //filled with zeros as needed
-    gameOver = false;
+  gameMatrix = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]; //filled with zeros as needed
+  gameOver = false;
 }
 
 function computerMove() {
-
-    //Prodji svih 7 (x) mogucih "trojki" koje dovode do pobede (ima ih 7)
-    for (var x = 0; x <= 7; x++) {
-        // Potrazi prvo da li negde imaju 2 oksa i trece prazno mesto i popuni ga sa oks
-        if (arraySum(gameMatrix, x) === -2) {
-            var zero1 = indexOfZero(gameMatrix, x);
-            gameMatrix[zero1[0]][zero1[1]] = -1;
-            return divMatrix[zero1[0]][zero1[1]];
-        }
+  //Prodji svih 7 (x) mogucih "trojki" koje dovode do pobede (ima ih 7)
+  for (var x = 0; x <= 7; x++) {
+    // Potrazi prvo da li negde imaju 2 oksa i trece prazno mesto i popuni ga sa oks
+    if (arraySum(gameMatrix, x) === -2) {
+      var zero1 = indexOfZero(gameMatrix, x);
+      gameMatrix[zero1[0]][zero1[1]] = -1;
+      return divMatrix[zero1[0]][zero1[1]];
     }
-    // Zatim potrazi da li negde imaju 2 iksa i trece prazno mesto i popuni ga sa oks
-    for (x = 0; x <= 7; x++) {
-        if (arraySum(gameMatrix, x) === 2) {
-            var zero2 = indexOfZero(gameMatrix, x);
-            gameMatrix[zero2[0]][zero2[1]] = -1;
-            return divMatrix[zero2[0]][zero2[1]];
-        }
+  }
+  // Zatim potrazi da li negde imaju 2 iksa i trece prazno mesto i popuni ga sa oks
+  for (x = 0; x <= 7; x++) {
+    if (arraySum(gameMatrix, x) === 2) {
+      var zero2 = indexOfZero(gameMatrix, x);
+      gameMatrix[zero2[0]][zero2[1]] = -1;
+      return divMatrix[zero2[0]][zero2[1]];
     }
-    //Ako ne, onda napravi random potez
-    return computerRandomMove();
+  }
+  //Ako ne, onda napravi random potez
+  return computerRandomMove();
 }
 
 function computerRandomMove() {
-    var randomRow = Math.floor((Math.random() * 3));
-    var randomColumn = Math.floor((Math.random() * 3));
+  var randomRow = Math.floor(Math.random() * 3);
+  var randomColumn = Math.floor(Math.random() * 3);
 
-    if (gameMatrix[randomRow][randomColumn] === 0) {
-        gameMatrix[randomRow][randomColumn] = -1;
-        return divMatrix[randomRow][randomColumn];
-    } else
-        return computerRandomMove();
+  if (gameMatrix[randomRow][randomColumn] === 0) {
+    gameMatrix[randomRow][randomColumn] = -1;
+    return divMatrix[randomRow][randomColumn];
+  } else return computerRandomMove();
 }
 
 function isTheBoardFull() {
-    for (var row = 0; row < 3; row++) {
-        for (var column = 0; column < 3; column++) {
-            if (gameMatrix[row][column] === 0) {
-                return false;
-            }
-        }
+  for (var row = 0; row < 3; row++) {
+    for (var column = 0; column < 3; column++) {
+      if (gameMatrix[row][column] === 0) {
+        return false;
+      }
     }
-    gameOver = true;
-    return true;
+  }
+  gameOver = true;
+  return true;
 }
 
 function didSome1Win(three) {
-    // Prodji svih 7 (x) mogucih "trojki" koje dovode do pobede (ima ih 7)
-    for (var x = 0; x <= 7; x++) {
-        if (arraySum(gameMatrix, x) === three) {
-            if (x >= 0 && x <= 2) {
-                for (var i = 0; i < 3; i++) {
-                    divMatrix[x][i].style.backgroundColor = "#a663ff";
-                }
-            } else if (x >= 3 && x <= 5) {
-                for (var i = 0; i < 3; i++) {
-                    divMatrix[i][x - 3].style.backgroundColor = "#a663ff";
-                }
-            } else if (x === 6) {
-                for (var i = 0; i < 3; i++) {
-                    divMatrix[i][i].style.backgroundColor = "#a663ff";
-                }
-            } else if (x === 7) {
-                for (var i = 0; i < 3; i++) {
-                    divMatrix[2 - i][i].style.backgroundColor = "#a663ff";
-                }
-            }
-            removeAllClicks();
-            gameOver = true;
-            if (three === 3) {
-                incrementPlayerScore();
-            } else if (three === -3) {
-                incrementComputerScore();
-            }
-            break;
+  // Prodji svih 7 (x) mogucih "trojki" koje dovode do pobede (ima ih 7)
+  for (var x = 0; x <= 7; x++) {
+    if (arraySum(gameMatrix, x) === three) {
+      if (x >= 0 && x <= 2) {
+        for (var i = 0; i < 3; i++) {
+          divMatrix[x][i].style.backgroundColor = COLOR_VICTORY;
         }
+      } else if (x >= 3 && x <= 5) {
+        for (var i = 0; i < 3; i++) {
+          divMatrix[i][x - 3].style.backgroundColor = COLOR_VICTORY;
+        }
+      } else if (x === 6) {
+        for (var i = 0; i < 3; i++) {
+          divMatrix[i][i].style.backgroundColor = COLOR_VICTORY;
+        }
+      } else if (x === 7) {
+        for (var i = 0; i < 3; i++) {
+          divMatrix[2 - i][i].style.backgroundColor = COLOR_VICTORY;
+        }
+      }
+      removeAllClicks();
+      gameOver = true;
+      if (three === 3) {
+        incrementPlayerScore();
+      } else if (three === -3) {
+        incrementComputerScore();
+      }
+      break;
     }
+  }
 
-    return gameOver;
+  return gameOver;
 }
-
 
 function _computerMove() {
-    var move = false;
-    if (gameOver === false) {
-        if (!didSome1Win(3) && !isTheBoardFull()) {
-            move = computerMove();
-            didSome1Win(-3);
-        }
+  var move = false;
+  if (gameOver === false) {
+    if (!didSome1Win(3) && !isTheBoardFull()) {
+      move = computerMove();
+      didSome1Win(-3);
     }
-    return move;
+  }
+  return move;
 }
-
-
--- htmlSetup.js
+++ htmlSetup.js
@@ -1,152 +1,160 @@
 var cells;
 var player = true;
 
-window.onload = function() {
-    cells = document.getElementsByClassName("table-cell");
-    resize();
-    window.onresize = resize;
-    setupNewGameButton();
-    newGame();
+var CELL_COLOR = "#AA00FF";
+var CELL_HOVER = "#D500F9";
+
+window.onload = function () {
+  cells = document.getElementsByClassName("table-cell");
+  resize();
+  window.onresize = resize;
+  setupNewGameButton();
+  newGame();
 };
 
 function resize() {
-    var cellWidth = cells[0].clientWidth;
-    for (var i = 0; i < cells.length; i++) {
-        var cell = cells[i];
-        cell.style.height = cellWidth + "px";
-        if (cell.hasChildNodes()) {
-            var canvas = cell.lastChild;
-            fitCanvasToDiv(canvas, cell);
-            if (canvas.id === "X") {
-                drawX();
-            } else if (canvas.id === "O") {
-                drawO();
-            }
-
-        }
+  var cellWidth = cells[0].clientWidth;
+  for (var i = 0; i < cells.length; i++) {
+    var cell = cells[i];
+    cell.style.height = cellWidth + "px";
+    if (cell.hasChildNodes()) {
+      var canvas = cell.lastChild;
+      fitCanvasToDiv(canvas, cell);
+      if (canvas.id === "X") {
+        drawX();
+      } else if (canvas.id === "O") {
+        drawO();
+      }
     }
+  }
 }
 
 function fitCanvasToDiv(canvas, div) {
-    canvas.width  = div.clientWidth * 0.8;
-    canvas.height = div.clientHeight * 0.8;
+  canvas.width = div.clientWidth * 0.8;
+  canvas.height = div.clientHeight * 0.8;
 }
 
-function mouseOver(event) { event.target.style.backgroundColor = "#90caf9"; }
+function mouseOver(event) {
+  event.target.style.backgroundColor = CELL_HOVER;
+}
 
-function mouseOut(event) { event.target.style.backgroundColor = "#64b5f6"; }
+function mouseOut(event) {
+  event.target.style.backgroundColor = CELL_COLOR;
+}
 
 function onClick(event) {
-    var element = event.target;
-    element.style.backgroundColor = "#64b5f6";
-    removeListeners(element);
-    setGameElm(element);
-    drawX(createAndAppendCanvas(element));
-    var answerElem = _computerMove();
-    if (answerElem) {
-        drawO(createAndAppendCanvas(answerElem));
-        removeListeners(answerElem);
-    }
+  var element = event.target;
+  element.style.backgroundColor = CELL_COLOR;
+  removeListeners(element);
+  setGameElm(element);
+  drawX(createAndAppendCanvas(element));
+  var answerElem = _computerMove();
+  if (answerElem) {
+    drawO(createAndAppendCanvas(answerElem));
+    removeListeners(answerElem);
+  }
 }
 
 function createAndAppendCanvas(element) {
-    var canvas = document.createElement('canvas');
-    fitCanvasToDiv(canvas, element);
-    element.appendChild(canvas);
-    return canvas;
+  var canvas = document.createElement("canvas");
+  fitCanvasToDiv(canvas, element);
+  element.appendChild(canvas);
+  return canvas;
 }
 
 function addListeners(element) {
-    element.addEventListener("mouseover", mouseOver, false);
-    element.addEventListener("mouseout", mouseOut, false);
-    element.addEventListener("click", onClick, false);
+  element.addEventListener("mouseover", mouseOver, false);
+  element.addEventListener("mouseout", mouseOut, false);
+  element.addEventListener("click", onClick, false);
 }
 
 function removeListeners(element) {
-    element.removeEventListener("mouseover", mouseOver);
-    element.removeEventListener("mouseout", mouseOut);
-    element.removeEventListener("click", onClick);
+  element.removeEventListener("mouseover", mouseOver);
+  element.removeEventListener("mouseout", mouseOut);
+  element.removeEventListener("click", onClick);
 }
 
 function setupHoverAndClick() {
-    for(var i = 0; i < cells.length; i++) {
-        addListeners(cells[i]);
-        cells[i].style.backgroundColor = "#64b5f6";
-    }
+  for (var i = 0; i < cells.length; i++) {
+    addListeners(cells[i]);
+    cells[i].style.backgroundColor = CELL_COLOR;
+  }
 }
 function newGame() {
-    for(var i = 0; i < cells.length; i++) {
-        var cell = cells[i];
-        while(cell.hasChildNodes()) cell.removeChild(cell.lastChild);
-    }
-    setupHoverAndClick();
-    _newGame();
+  for (var i = 0; i < cells.length; i++) {
+    var cell = cells[i];
+    while (cell.hasChildNodes()) cell.removeChild(cell.lastChild);
+  }
+  setupHoverAndClick();
+  _newGame();
 }
 
 function setupNewGameButton() {
-    var btn = document.getElementById("btn");
+  var btn = document.getElementById("btn");
 
-    btn.addEventListener("click", newGame, false);
+  btn.addEventListener("click", newGame, false);
 }
 
 function drawX(canvas) {
-    var draw = new InitDraw(canvas);
-    draw.drawX();
+  var draw = new InitDraw(canvas);
+  draw.drawX();
 }
 function drawO(canvas) {
-    var draw = new InitDraw(canvas);
-    draw.drawO();
+  var draw = new InitDraw(canvas);
+  draw.drawO();
 }
 
 function InitDraw(canvas) {
-    this.ctx = canvas.getContext("2d");
+  this.canvas = canvas;
+  this.ctx = canvas.getContext("2d");
 
-    this.a = canvas.height / 3;
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
-    this.colors = ["gray", "black"];
-
-    this.drawX = function() {
-        canvas.id = "X";
-        for (var i = 1; i < 3; i++) {
-            this.ctx.lineWidth = 2*this.a / (i*10);
-            this.ctx.strokeStyle = this.colors[i-1];
-            this.ctx.beginPath();
-            this.ctx.lineCap = "round";
-            this.ctx.moveTo(this.x - this.a, this.y - this.a);
-            this.ctx.lineTo(this.x + this.a, this.y + this.a);
-            this.ctx.moveTo(this.x + this.a, this.y - this.a);
-            this.ctx.lineTo(this.x - this.a, this.y + this.a);
-            this.ctx.stroke();
-        }
-    };
-
-    this.drawO = function() {
-        canvas.id = "O";
-        for (var i = 1; i < 3; i++) {
-            this.ctx.lineWidth = 2*this.a / (i*10);
-            this.ctx.strokeStyle = this.colors[i-1];
-            this.ctx.beginPath();
-            this.ctx.arc(this.x, this.y, this.a, 0, 2 * Math.PI);
-            this.ctx.stroke();
-        }
-    };
+  this.a = canvas.height / 3;
+  this.x = canvas.width / 2;
+  this.y = canvas.height / 2;
 }
 
-function setGameElm( elm ){
-    for(var row = 0; row < 3; row++) {
-        for(var col = 0; col < 3; col++) {
-            if( divMatrix[row][col] === elm ) {
-                gameMatrix[row][col] = 1;
-            }
-        }
+InitDraw.prototype.colors = ["white", "white"];
+
+InitDraw.prototype.drawX = function () {
+  this.canvas.id = "X";
+  for (var i = 1; i < 3; i++) {
+    this.ctx.lineWidth = (2 * this.a) / (i * 10);
+    this.ctx.strokeStyle = this.colors[i - 1];
+    this.ctx.beginPath();
+    this.ctx.lineCap = "round";
+    this.ctx.moveTo(this.x - this.a, this.y - this.a);
+    this.ctx.lineTo(this.x + this.a, this.y + this.a);
+    this.ctx.moveTo(this.x + this.a, this.y - this.a);
+    this.ctx.lineTo(this.x - this.a, this.y + this.a);
+    this.ctx.stroke();
+  }
+};
+
+InitDraw.prototype.drawO = function () {
+  this.canvas.id = "O";
+  for (var i = 1; i < 3; i++) {
+    this.ctx.lineWidth = (2 * this.a) / (i * 10);
+    this.ctx.strokeStyle = this.colors[i - 1];
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.a, 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+};
+
+function setGameElm(elm) {
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      if (divMatrix[row][col] === elm) {
+        gameMatrix[row][col] = 1;
+      }
     }
+  }
 }
 
 function removeAllClicks() {
-    for (var row = 0; row < 3; row++) {
-        for (var col = 0; col < 3; col++) {
-            removeListeners(divMatrix[row][col]);
-        }
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      removeListeners(divMatrix[row][col]);
     }
+  }
 }
